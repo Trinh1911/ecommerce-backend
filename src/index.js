e2a13e37a5a5cd2a98c58ebafcc5a3be861c0411
Trinh1911 <nguyenkimtrinhntn@gmail.com>
2023-09-03 23:42:41 +0700

    video 17

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const routes = require('./routes');
+const bodyParser = require('body-parser');
 dotenv.config()
 const app = express();
 const port = process.env.PORT || 3001
-app.get('/', (req, res) => {
-     res.send('hello world')
-})
-mongoose.connect(`mongodb+srv://kimtrinh:${process.env.MONGO_DB}@cluster0.yqrzhit.mongodb.net/?retryWrites=true&w=majority`)
-.then(()=> {
-    console.log('connect db successfully')
-})
-.catch((err)=> {
-    console.log(err)
-})
+app.use(bodyParser.json())
+routes(app);
+mongoose.connect(`${process.env.MONGO_DB}`)
+    .then(() => {
+        console.log('connect db successfully')
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 app.listen(port, () => {
     console.log('listening on port', + port)
 })
