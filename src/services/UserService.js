0d762ecc74208363ed8ce7447024a8fab35f6b21
Trinh1911 <nguyenkimtrinhntn@gmail.com>
2023-09-06 22:54:11 +0700

    video 18 complete

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -3,6 +3,14 @@ const createUser = (newUser) => {
     return new Promise(async (resolve, reject) => {
         const { name, email, password, confirmPassword, phone } = newUser
         try {
+            const checkUser = await User.findOne({email: email})
+            // kiem tra user co trung khong
+            if(checkUser !== null) {
+                resolve({
+                    status: "OK",
+                    message: "the user is already"
+                })
+            }
             const createdUser = await User.create({
                 name, email, password, confirmPassword, phone
             })
