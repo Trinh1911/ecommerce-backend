63a6e7dcdad16da8bf2984c721f0e28646c2bf17
Trinh1911 <nguyenkimtrinhntn@gmail.com>
2023-09-06 21:33:24 +0700

    bug video 18

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,7 +1,18 @@
-const createUser = () => {
-    return new Promise((resolve, reject) => {
+const User = require('../models/UserModel');
+const createUser = (newUser) => {
+    return new Promise(async (resolve, reject) => {
+        const { name, email, password, confirmPassword, phone } = newUser
         try {
-        
+            const createdUser = await User.create({
+                name, email, password, confirmPassword, phone
+            })
+            if (createdUser) {
+                resolve({
+                    status: "OK",
+                    message: "SUCCESS",
+                    data: createdUser
+                })
+            }
         }
         catch (e) {
             reject(e);
