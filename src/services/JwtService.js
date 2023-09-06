4326c8f30e59a32e5996726ab7ed90494e4fdd48
Trinh1911 <nguyenkimtrinhntn@gmail.com>
2023-09-06 23:53:12 +0700

    video 19

---
+++ src/services/JwtService.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken')
+const genneralAccessToken = async (payload) => {
+    const access_token = jwt.sign(
+        {
+            payload
+        }, 'access_token', { expiresIn: '1h' }
+    )
+    return access_token
+}
+const genneralRefresToken = async (payload) => {
+    const access_token = jwt.sign(
+        {
+            payload
+        }, 'refresh_token', { expiresIn: '365d' }
+    )
+    return access_token
+}
+module.exports = {
+    genneralAccessToken,genneralRefresToken
+}
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,18 +1,23 @@
 const User = require('../models/UserModel');
+const bcrypt = require('bcrypt');
+const { genneralAccessToken, genneralRefresToken } = require("./JwtService")
 const createUser = (newUser) => {
     return new Promise(async (resolve, reject) => {
         const { name, email, password, confirmPassword, phone } = newUser
         try {
-            const checkUser = await User.findOne({email: email})
+            const checkUser = await User.findOne({ email: email })
             // kiem tra user co trung khong
-            if(checkUser !== null) {
+            if (checkUser !== null) {
                 resolve({
                     status: "OK",
                     message: "the user is already"
                 })
             }
+            // ma hoa mat khau
+            const hash = bcrypt.hashSync(password, 10)
+            console.log('hash', hash)
             const createdUser = await User.create({
-                name, email, password, confirmPassword, phone
+                name, email, password: hash, phone
             })
             if (createdUser) {
                 resolve({
@@ -27,6 +32,45 @@ const createUser = (newUser) => {
         }
     })
 }
+const loginUser = (userLogin) => {
+    return new Promise(async (resolve, reject) => {
+        const { name, email, password, confirmPassword, phone } = userLogin
+        try {
+            const checkUser = await User.findOne({ email: email })
+            // kiem tra user co trung khong
+            if (checkUser === null) {
+                resolve({
+                    status: "OK",
+                    message: "the user is not defined"
+                })
+            }
+            const comparePassword = bcrypt.compareSync(password, checkUser.password)
+            if (!comparePassword) {
+                resolve({
+                    status: "OK",
+                    message: "the password or user is incorrect"
+                })
+            }
+            const access_token = await genneralAccessToken({
+                id: checkUser.id,
+                isAdmin: checkUser.isAdmin
+            })
+            const refresh_token = await genneralRefresToken({
+                id: checkUser.id,
+                isAdmin: checkUser.isAdmin
+            })
+            resolve({
+                status: "OK",
+                message: "SUCCESS",
+                access_token,
+                refresh_token
+            })
+        }
+        catch (e) {
+            reject(e);
+        }
+    })
+}
 module.exports = {
-    createUser
+    createUser, loginUser
 }
