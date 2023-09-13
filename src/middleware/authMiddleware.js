32f6031495f35f65c22ced10f052f63c55a4cd5d
Trinh1911 <nguyenkimtrinhntn@gmail.com>
2023-09-13 12:10:53 +0700

    video 23

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -21,6 +21,27 @@ const authMiddleware = (req, res, next) => {
         }
     })
 }
+const authUerMiddleware = (req, res, next) => {
+    const token =  req.headers.token.split(' ')[1]
+    const userId =  req.params.id
+    jwt.verify(token, process.env.ACCESSS_TOKEN, function (err, user) {
+        if(err) {
+            return res.status(404).json({
+                message: 'The authentication',
+                status: 'ERROR'
+            })
+        }
+        const { payload } = user
+        if(payload?.isAdmin || payload?.id === userId) {
+            next()
+        }else {
+            return res.status(404).json({
+                message: 'The authentication',
+                status: 'ERROR'
+            })
+        }
+    })
+}
 module.exports = {
-    authMiddleware
+    authMiddleware, authUerMiddleware
 }
--- src/services/JwtService.js
+++ src/services/JwtService.js
@@ -1,11 +1,11 @@
 const jwt = require('jsonwebtoken')
-const dotenv =  require('dotenv')
+const dotenv = require('dotenv')
 dotenv.config()
 const genneralAccessToken = async (payload) => {
     const access_token = jwt.sign(
         {
             payload
-        }, process.env.ACCESSS_TOKEN, { expiresIn: '1h' }
+        }, process.env.ACCESSS_TOKEN, { expiresIn: '30s' }
     )
     return access_token
 }
@@ -17,7 +17,38 @@ const genneralRefresToken = async (payload) => {
     )
     return refresh_token
 }
-
+const refresTokenJwtService = (token) => {
+    return new Promise((resolve, reject) => {
+        try {
+            console.log('token', token)
+            jwt.verify(token, process.env.REFRESH_TOKEN, async (err, user) => {
+                if (err) {
+                    console.log('err', err)
+                    resolve(
+                        {
+                            status: 'ERROR',
+                            message: 'the authentication'
+                        }
+                    )
+                }
+                const { payload } = user
+                const access_token = await genneralAccessToken({
+                    id: payload?.id,
+                    isAdmin: payload?.isAdmin
+                })
+                console.log('access token', access_token)
+                resolve({
+                    status: "OK",
+                    message: "SUCCESS",
+                    access_token: access_token
+                })
+            })
+        }
+        catch (e) {
+            reject(e);
+        }
+    })
+}
 module.exports = {
-    genneralAccessToken,genneralRefresToken
+    genneralAccessToken, genneralRefresToken, refresTokenJwtService
 }
